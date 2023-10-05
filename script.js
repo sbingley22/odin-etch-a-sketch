86b3e8609bef856ca741e6a63ce67974cd23bae0
sbingley22 <sbingley22@gmail.com>
2023-10-05 14:27:54 +0100

    basic version complete

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("heelo")
+function spawnGrid(size){
+    for (i=0; i<size; i++){
+        row = document.createElement("div")
+        row.classList.add("grid-row")
+        for (j=0; j<size; j++){
+            square = document.createElement("div")
+            square.classList.add("grid-square")
+            square.addEventListener("mouseenter", (e) => {
+                e.target.style.backgroundColor = "#AAAAAA"
+            })
+            row.appendChild(square)
+        }
+        gridContainer.appendChild(row)
+    }
+}
+
+function removeGrid() {
+    gridContainer.innerHTML = ""
+}
+
+function getGridInput(e) {
+    newSize = inputField.value
+    if (newSize > 0 && newSize <= 100){
+        removeGrid()
+        spawnGrid(newSize)
+    }
+}
+
+mainContainer = document.getElementById("main-container")
+
+inputField = document.createElement("input")
+newGridBtn = document.createElement("button")
+newGridBtn.textContent = "New Grid"
+newGridBtn.addEventListener("click", getGridInput)
+mainContainer.appendChild(inputField)
+mainContainer.appendChild(newGridBtn)
+
+gridContainer = document.createElement("div")
+gridContainer.classList.add("grid-container")
+mainContainer.appendChild(gridContainer)
+
+spawnGrid(16)
